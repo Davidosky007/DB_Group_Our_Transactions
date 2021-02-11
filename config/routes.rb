675afb832d7d823e4c61a8e984c9949d9e7a5b3a
David Bassey <davidosky1@gmail.com>
2021-02-11 16:00:27 +0100

    modifies routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+  get '/login', to: 'users#login'
+  post '/login', to: 'users#logincreate'
+  delete '/logout', to: 'users#logout'
+  get '/transactions/no-grp', to: 'transactions#no_grp'
+  resources :users, only: %i[create show]
+  resources :groups, only: %i[index show new create]
+  resources :transactions, only: %i[index show new create]
+  root 'static_pages#home'
 end

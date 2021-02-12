c641f2bb2c26134309ac6f8a71c21bdfec06c217
David Bassey <davidosky1@gmail.com>
2021-02-12 14:27:07 +0100

    fixes linter error in user_feature_spec.rb file

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -6,24 +6,20 @@ RSpec.feature 'UserFeatures', type: :feature do
       visit login_path
       expect(page).to have_content 'LOGIN'
     end
-
     it 'can load a signup page' do
       visit signup_path
       expect(page).to have_content 'SIGNUP'
     end
-
     it 'show a home page' do
       visit root_path
       expect(page).to have_css('h1', text: 'Group Our Transactions')
     end
-
     it 'can create a new user' do
       visit signup_path
       fill_in 'Name', with: 'User'
       click_button 'Create User'
       expect(page).to have_content 'Account created successfully!'
     end
-
     it 'can log in a user' do
       create_user
       visit login_path
@@ -32,7 +28,6 @@ RSpec.feature 'UserFeatures', type: :feature do
       expect(page).to have_content 'you are logged in!'
     end
   end
-
   context 'logged in user' do
     it "can view it's own profile" do
       log_in
